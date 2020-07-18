fca7b80230a5300b625463ea04c2c3876c67eb07
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 20:31:11 -0600

    Update comedians index spec to use an age query param

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe "Comedians Index Spec" do
     @spooktober2 = @casper.tv_specials.create!(name: "Spook-Tober! Encore!", run_time: 120 )
 
     @spidertober = @spiderman.tv_specials.create!(name: "Spiderman: The Comedy!", run_time: 90 )
-
-    visit "/comedians"
   end
 
   describe "As a visitor" do
     describe "When I visit /comedians" do
       it "I see a list of comedians' Name, Age, and City" do
+        visit "/comedians"
+
         within(".casper") do
           expect(page).to have_content("Name: #{@casper.name}")
           expect(page).to have_content("Age: #{@casper.age}")
@@ -32,6 +32,8 @@ RSpec.describe "Comedians Index Spec" do
       end
 
       it "I see a list of each comedian's TV specials' names and run times" do
+        visit "/comedians"
+
         within(".casper") do
           expect(page).to have_content("#{@casper.name}'s TV Specials:")
           expect(page).to have_content("#{@spooktober1.name} (#{@spooktober1.run_time} mins)")
@@ -45,16 +47,33 @@ RSpec.describe "Comedians Index Spec" do
       end
 
       it "I see a thumbnail image for each comedian" do
+        visit "/comedians"
+
         within(".casper") do
           expect(page).to have_css("img[src*='generic-image-placeholder']")
           expect(page).to have_css("img[alt='#{@casper.name.downcase}-image']")
         end
+
         within(".spiderman") do
           expect(page).to have_css("img[src*='generic-image-placeholder']")
           expect(page).to have_css("img[alt='#{@spiderman.name.downcase}-image']")
         end
       end
     end
-  end
 
+    describe "When I visit /comedians with an age query param" do
+      it "I only see comedians who match the age criteria" do
+        visit "/comedians?age=100"
+
+        expect(page).to have_css(".casper")
+        expect(page).to_not have_css(".spiderman")
+
+        visit "/comedians?age=28"
+
+        expect(page).to have_css(".spiderman")
+        expect(page).to_not have_css(".casper")
+      end
+    end
+
+  end
 end
