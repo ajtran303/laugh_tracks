62909e51c85b5f44afc0be53fdd71e17877e20cf
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 20:45:30 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+image_url = "https://www.webfx.com/blog/images/cdn.designinstruct.com/files/582-how-to-image-placeholders/generic-image-placeholder.png"
+
+casper = Comedian.create!(name: "Casper", age: 100, city: "Boston", image_url: image_url)
+spiderman = Comedian.create!(name: "Spiderman", age: 28, city: "New York City", image_url: image_url)
+
+spooktober1 = casper.tv_specials.create!(name: "Spook-Tober!", run_time: 80 )
+spooktober2 = casper.tv_specials.create!(name: "Spook-Tober! Encore!", run_time: 120 )
+
+spidertober = spiderman.tv_specials.create!(name: "Spiderman: The Comedy!", run_time: 90 )
