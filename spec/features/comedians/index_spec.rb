a5f370c0d9e8a4b44e742fe99f3ac17f1302824a
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 20:04:09 -0600

    Add spec for listing comedians' tv specials on the comedians index page

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -4,13 +4,18 @@ RSpec.describe "Comedians Index Spec" do
   before :each do
     @casper = Comedian.create!(name: "Casper", age: 100, city: "Boston")
     @spiderman = Comedian.create!(name: "Spiderman", age: 28, city: "New York City")
+
+    @spooktober1 = @casper.tv_specials.create!(name: "Spook-Tober!", run_time: 80 )
+    @spooktober2 = @casper.tv_specials.create!(name: "Spook-Tober! Encore!", run_time: 120 )
+
+    @spidertober = @spiderman.tv_specials.create!(name: "Spiderman: The Comedy!", run_time: 90 )
+
+    visit "/comedians"
   end
 
   describe "As a visitor" do
     describe "When I visit /comedians" do
       it "I see a list of comedians' Name, Age, and City" do
-        visit "/comedians"
-
         within(".casper") do
           expect(page).to have_content("Name: #{@casper.name}")
           expect(page).to have_content("Age: #{@casper.age}")
@@ -21,6 +26,19 @@ RSpec.describe "Comedians Index Spec" do
           expect(page).to have_content("Name: #{@spiderman.name}")
           expect(page).to have_content("Age: #{@spiderman.age}")
           expect(page).to have_content("City: #{@spiderman.city}")
+        end
+      end
+
+      it "I see a list of each comedian's TV specials' names and run times" do
+        within(".casper") do
+          expect(page).to have_content("#{@casper.name}'s TV Specials:")
+          expect(page).to have_content("#{@spooktober1.name} (#{@spooktober1.run_time} mins)")
+          expect(page).to have_content("#{@spooktober2.name} (#{@spooktober2.run_time} mins)")
+        end
+
+        within(".spiderman") do
+          expect(page).to have_content("#{@spiderman.name}'s TV Specials:")
+          expect(page).to have_content("#{@spidertober.name} (#{@spidertober.run_time} mins)")
         end
       end
     end
