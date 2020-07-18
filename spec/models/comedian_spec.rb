59c77368c42493998504b4d8a7bfd36d78e76030
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 19:53:14 -0600

    Add validation tests for comedian and tv special models

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Comedian do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_numericality_of :age }
+    it { should validate_presence_of :city }
+  end
+
   describe "associations" do
     it { should have_many :tv_specials }
   end
--- spec/models/tv_special_spec.rb
+++ spec/models/tv_special_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 RSpec.describe TvSpecial do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_numericality_of :run_time }
+  end
+
   describe "associations" do
     it { should belong_to :comedian }
   end
