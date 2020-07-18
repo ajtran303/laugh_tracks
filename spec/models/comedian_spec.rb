98e7a5b8ee761d01274ceae19f0b30853ca84602
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 22:43:16 -0600

    Add model method tests for Comedian

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Comedian do
   describe "associations" do
     it { should have_many :tv_specials }
   end
+
+  describe "methods" do
+    image_url = "https://www.webfx.com/blog/images/cdn.designinstruct.com/files/582-how-to-image-placeholders/generic-image-placeholder.png"
+
+    Comedian.create!(name: "Sasper", age: 100, city: "Boston", image_url: image_url)
+    Comedian.create!(name: "Cpiderman", age: 28, city: "New York City", image_url: image_url)
+
+    it ".average_age" do
+      expect(Comedian.average_age).to eq(64)
+    end
+
+    it ".cities" do
+      expect(Comedian.cities).to eq(["Boston", "New York City"])
+    end
+  end
 end
