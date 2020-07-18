a0ddb06dfab40f7e8f2023ed127b71ffb1d14379
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 19:53:35 -0600

    Add validation for comedian and tv special models

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,6 @@
 class Comedian < ApplicationRecord
+  validates_presence_of :name, :city
+  validates_numericality_of :age
+
   has_many :tv_specials
 end
--- app/models/tv_special.rb
+++ app/models/tv_special.rb
@@ -1,3 +1,6 @@
 class TvSpecial < ApplicationRecord
+  validates_presence_of :name
+  validates_numericality_of :run_time
+
   belongs_to :comedian
 end
